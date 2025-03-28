d4a96a0a4402fd88ff64f007ff6b62616011f827
Arshu <arshu@coresoftint.com>
2025-03-28 14:27:28 +0545

    section vh is blank

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -117,7 +117,7 @@ const Hero = () => {
 
   return (
     <section 
-  className="relative w-full min-h-screen sm:h-[90vh] overflow-hidden bg-cover bg-center flex items-center"
+  className="relative w-full min-h-screen sm:h-[vh] overflow-hidden bg-cover bg-center flex items-center"
   style={{
     backgroundImage: `url('https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2244&q=80')`,
     backgroundSize: "cover",
